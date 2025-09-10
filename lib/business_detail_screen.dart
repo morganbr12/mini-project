03144a4f786dc11d698a07e778d99245b94822ec
Morganbr12 <f.morgan221@outlook.com>
2025-09-10 20:46:47 +0000

    this is the first commit which is has the scaffold of all the screens

---
+++ lib/business_detail_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BusinessDetailScreen extends StatelessWidget {
+  const BusinessDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Business Detail')),
+      body: const Center(child: Text('Business Detail Placeholder')),
+    );
+  }
+}
---
+++ lib/business_list_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BusinessListScreen extends StatelessWidget {
+  const BusinessListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Businesses')),
+      body: const Center(child: Text('Business List Placeholder')),
+    );
+  }
+}
